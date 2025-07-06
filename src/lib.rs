ced91a09ed0f5aa8f122da05c63b0a79902153dd
Gusto <bacvinka@gmail.com>
2025-07-06 06:32:23 +0300

    Use rustix instead of mio

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,6 @@ pub mod term;
 
 use std::{os::fd::OwnedFd, sync::OnceLock};
 
-use mio::Token;
-
-const STDIN_TOKEN: Token = Token(0);
-const PTY_TOKEN: Token = Token(1);
-const PIPE_TOKEN: Token = Token(2);
-const RESIZE_TOKEN: Token = Token(3);
-
 pub const WYE_SESSION_VAR: &str = "WYE";
 const WYE_PIPE_DIR: &str = "/tmp";
 const WYE_PIPE_PREFIX: &str = "wye";
--- src/parent.rs
+++ src/parent.rs
@@ -2,20 +2,23 @@ use std::{
     cmp::Ordering,
     fs::File,
     io::{self, Read, Write},
-    os::fd::{AsRawFd, OwnedFd},
+    os::fd::{AsFd, OwnedFd},
     path::Path,
 };
 
-use mio::{Events, Interest, Poll, unix::SourceFd};
 use nix::{
     fcntl::{F_SETFL, OFlag, fcntl, open},
-    libc::{STDIN_FILENO, c_int},
+    libc::c_int,
     sys::{signal, stat::Mode},
     unistd::mkfifo,
 };
+use rustix::{
+    event::{PollFd, PollFlags, pause, poll},
+    io::dup,
+};
 
 use crate::{
-    PIPE_TOKEN, PTY_TOKEN, RESIZE_OUT, RESIZE_TOKEN, STDIN_TOKEN, WYE_PIPE_DIR, WYE_PIPE_PREFIX,
+    RESIZE_OUT, WYE_PIPE_DIR, WYE_PIPE_PREFIX,
     config::Config,
     log::{log_closed_session, log_opened_session},
     term::{get_winsize, update_pty_size},
@@ -36,47 +39,18 @@ fn prepare_pipe(path: &str) -> io::Result<OwnedFd> {
     Ok(pipe_fd)
 }
 
-fn setup_master(poll: &mut Poll, master_fd: OwnedFd) -> io::Result<File> {
-    let master_raw = master_fd.as_raw_fd();
-    let mut master_source = SourceFd(&master_raw);
-    poll.registry()
-        .register(&mut master_source, PTY_TOKEN, Interest::READABLE)?;
-    Ok(File::from(master_fd))
-}
-
-fn setup_stdin(poll: &mut Poll) -> io::Result<io::Stdin> {
-    let mut stdin_source = SourceFd(&STDIN_FILENO);
-    poll.registry()
-        .register(&mut stdin_source, STDIN_TOKEN, Interest::READABLE)?;
-    Ok(io::stdin())
-}
-
-fn setup_pipe(poll: &mut Poll, pipe_fd: OwnedFd) -> io::Result<File> {
-    let pipe_raw = pipe_fd.as_raw_fd();
-    let mut pipe_source = SourceFd(&pipe_raw);
-    poll.registry()
-        .register(&mut pipe_source, PIPE_TOKEN, Interest::READABLE)?;
-    Ok(File::from(pipe_fd))
-}
-
-fn setup_resize(poll: &mut Poll, signal_in: OwnedFd) -> io::Result<File> {
+fn setup_resize(signal_in: &OwnedFd) -> io::Result<()> {
     let sig_action = signal::SigAction::new(
         signal::SigHandler::Handler(handle_sigwinch),
         signal::SaFlags::empty(),
         signal::SigSet::empty(),
     );
     unsafe { signal::sigaction(signal::Signal::SIGWINCH, &sig_action)? };
-    fcntl(&signal_in, F_SETFL(OFlag::O_NONBLOCK))?;
-
-    let signal_fd = signal_in.as_raw_fd();
-    let mut signal_source = SourceFd(&signal_fd);
-    poll.registry()
-        .register(&mut signal_source, RESIZE_TOKEN, Interest::READABLE)?;
-    Ok(File::from(signal_in))
+    fcntl(signal_in, F_SETFL(OFlag::O_NONBLOCK))?;
+    Ok(())
 }
 
 fn event_loop(
-    poll: &mut Poll,
     stdin: &mut io::Stdin,
     master_file: &mut File,
     pipe_file: &mut File,
@@ -89,11 +63,22 @@ fn event_loop(
     let stdout = io::stdout();
     let mut stdout_lock = stdout.lock();
 
-    let mut events = Events::with_capacity(128);
+    let poll_stdin = dup(stdin.as_fd())?;
+    let poll_master = dup(master_file.as_fd())?;
+    let poll_pipe = dup(pipe_file.as_fd())?;
+    let poll_resize = dup(resize_file.as_fd())?;
+
+    let mut poll_fds = [
+        PollFd::new(&poll_stdin, PollFlags::IN),
+        PollFd::new(&poll_master, PollFlags::IN),
+        PollFd::new(&poll_pipe, PollFlags::IN),
+        PollFd::new(&poll_resize, PollFlags::IN),
+    ];
 
     loop {
-        match poll.poll(&mut events, None) {
-            Ok(_) => {}
+        match poll(&mut poll_fds, None) {
+            Ok(0) => pause(), // No new events, will wake up.
+            Ok(_) => {}       // Got something, proceed to event loop.
             Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {
                 continue;
             }
@@ -102,69 +87,62 @@ fn event_loop(
             }
         }
 
-        for event in events.iter() {
-            match event.token() {
-                STDIN_TOKEN => {
-                    let n = stdin.read(&mut stdin_buf)?;
-                    match n.cmp(&0) {
-                        Ordering::Greater => {
-                            master_file.write_all(&stdin_buf[..n])?;
-                        }
-                        Ordering::Equal => {
-                            return Ok(());
-                        }
-                        Ordering::Less => {
-                            return Err(io::Error::last_os_error().into());
-                        }
-                    }
+        if poll_fds[0].revents().contains(PollFlags::IN) {
+            let n = stdin.read(&mut stdin_buf)?;
+            match n.cmp(&0) {
+                Ordering::Greater => {
+                    master_file.write_all(&stdin_buf[..n])?;
                 }
-                PTY_TOKEN => {
-                    let n = master_file.read(&mut pty_buf)?;
-                    if n > 0 {
-                        stdout_lock.write_all(&pty_buf[..n])?;
-                        stdout_lock.flush()?;
-                    } else {
-                        return Ok(());
-                    }
+                Ordering::Equal => {
+                    return Ok(());
                 }
-                PIPE_TOKEN => {
-                    let n = pipe_file.read(&mut pipe_buf)?;
-                    if n > 0 {
-                        master_file.write_all(&pipe_buf[..n])?;
-                    }
+                Ordering::Less => {
+                    return Err(io::Error::last_os_error().into());
                 }
-                RESIZE_TOKEN => {
-                    let mut drain_buf = [0; 1];
-                    _ = resize_file.read(&mut drain_buf);
+            }
+        }
 
-                    if let Ok(new_size) = get_winsize() {
-                        let _ = update_pty_size(master_file, &new_size);
-                    }
-                }
-                _ => {}
+        if poll_fds[1].revents().contains(PollFlags::IN) {
+            let n = master_file.read(&mut pty_buf)?;
+            if n > 0 {
+                stdout_lock.write_all(&pty_buf[..n])?;
+                stdout_lock.flush()?;
+            } else {
+                return Ok(());
+            }
+        }
+        if poll_fds[2].revents().contains(PollFlags::IN) {
+            let n = pipe_file.read(&mut pipe_buf)?;
+            if n > 0 {
+                master_file.write_all(&pipe_buf[..n])?;
+            }
+        }
+        if poll_fds[2].revents().contains(PollFlags::IN) {
+            let mut drain_buf = [0; 1];
+            _ = resize_file.read(&mut drain_buf);
+
+            if let Ok(new_size) = get_winsize() {
+                let _ = update_pty_size(master_file, &new_size);
             }
         }
     }
 }
 
 pub fn process(config: Config, master_fd: OwnedFd) -> Result<(), Box<dyn std::error::Error>> {
-    let mut poll = Poll::new()?;
-
-    let mut master_file = setup_master(&mut poll, master_fd)?;
-    let mut stdin = setup_stdin(&mut poll)?;
+    let mut master_file = File::from(master_fd);
 
     let pipe_path = format!("{WYE_PIPE_DIR}/{WYE_PIPE_PREFIX}-{}", config.session_number);
     let pipe_fd = prepare_pipe(&pipe_path)?;
-    let mut pipe_file = setup_pipe(&mut poll, pipe_fd)?;
+    let mut pipe_file = File::from(pipe_fd);
 
     let (resize_in, resize_out) = nix::unistd::pipe()?;
     RESIZE_OUT.get_or_init(|| resize_out);
-    let mut resize_file = setup_resize(&mut poll, resize_in)?;
+    setup_resize(&resize_in)?;
+    let mut resize_file = File::from(resize_in);
 
     log_opened_session(config.session_number, &pipe_path)?;
     let res = event_loop(
-        &mut poll,
-        &mut stdin,
+        &mut io::stdin(),
         &mut master_file,
         &mut pipe_file,
         &mut resize_file,
